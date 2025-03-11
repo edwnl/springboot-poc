f344089201b07edd665055188625acf95c29ecb3
ed <edwinli123456@gmail.com>
2025-03-11 16:10:10 +1100

    switching to gradle

--- backend/src/main/java/com/edwn/poc/controller/HelloController.java
+++ backend/src/main/java/com/edwn/poc/controller/HelloController.java
@@ -1,14 +1,16 @@
+// backend/src/main/java/com/edwn/poc/controller/HelloController.java
 package com.edwn.poc.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
+@RequestMapping("/api")
 public class HelloController {
 
-    @GetMapping("/api/hello")
-    public String sayHello() {
+    @GetMapping("/hello")
+    public String hello() {
         return "Hello World from Spring Boot!";
     }
 }
