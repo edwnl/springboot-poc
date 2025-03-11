bb33820fffcd0c8a1dc3b65c63fcb32584e21e29
ed <edwinli123456@gmail.com>
2025-03-11 13:19:54 +1100

    setup simple spring project with a hello world controller

---
+++ backend/src/main/java/com/edwn/poc/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.edwn.poc.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/api/hello")
+    public String sayHello() {
+        return "Hello World from Spring Boot!";
+    }
+}
